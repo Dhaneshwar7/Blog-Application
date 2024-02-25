5f84328691408666a4b705cdd1bffed9b1ac50f2
Delta Mike <87529812+DELTAMIKE7@users.noreply.github.com>
2024-02-25 18:46:27 +0530

    redux store config done and components header footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
+import { login, logout } from './store/authSlice';
+import { Footer, Header } from './components/index';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+	// console.log(import.meta.env.VITE_APPWRITE_URL);
+	const [loading, setLoading] = useState(true);
+	const dispatch = useDispatch();
 
-  return (
-   <>
-   <div>Creating a write app</div></>
-  )
+	useEffect(() => {
+		authService
+			.getCurrentUser()
+			.then(userData => {
+				if (userData) {
+					dispatch(login({ userData }));
+					console.log('login');
+				} else {
+					dispatch(logout());
+					console.log('logout');
+				}
+			})
+			.finally(() => setLoading(false));
+	}, []);
+
+	return !loading ? (
+		<div className="min-h-screen w-screen flex flex-wrap justify-center items-center bg-slate-400">
+			<div className="w-full block">
+				<Header />
+				<main>{/* <Outlet/> */}</main>
+				<Footer />
+			</div>
+		</div>
+	) : null;
 }
 
-export default App
+export default App;
