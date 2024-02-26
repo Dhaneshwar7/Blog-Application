1462a9e0789d1cf3b70771d20d015c3f87f6a4fd
Delta Mike <87529812+DELTAMIKE7@users.noreply.github.com>
2024-02-26 16:47:27 +0530

    Components created

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,10 @@
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
+import LogoutBtn from './Header/LogoutBtn';
+import Container from './container/container';
+import Logo from './Logo';
+import Select from './Select';
+import PostCard from './PostCard';
+import Input from './Input';
 
-export { Header, Footer };
+export { Header, Footer, LogoutBtn, Container, Logo, Select, PostCard, Input };
